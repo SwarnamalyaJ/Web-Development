cc310d56316a86731dc2bd15634f0cabacf840e3
Swarnamalya <swarnamalyaj3@gmail.com>
2024-12-20 20:28:16 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-
-
-class ckdForm(forms.ModelForm):
-    class Meta():
-        model=ckdModel
-        fields=['Customer_Age','Gender','Education_Level','Income_Category','Card_Category','Credit_Limit','Total_Trans_Amt']
